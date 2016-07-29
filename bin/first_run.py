456055c72ffb67a623400b2f676e985915b1408f
tim <tim@hurricanelabs.com>
2016-07-29 13:13:31 -0400

    update first_run.py to account for updated name

--- bin/first_run.py
+++ bin/first_run.py
@@ -2,8 +2,8 @@ import os
 import shutil
 splunk_home = os.environ.get("SPLUNK_HOME")
 if splunk_home:
-    lookupfile = os.path.join(splunk_home, "etc", "apps", "broken_sources", "lookups", "expectedTime.csv")
-    defaultfile = os.path.join(splunk_home, "etc", "apps", "broken_sources", "lookups", "expectedTime.csv.default")
+    lookupfile = os.path.join(splunk_home, "etc", "apps", "broken_hosts", "lookups", "expectedTime.csv")
+    defaultfile = os.path.join(splunk_home, "etc", "apps", "broken_hosts", "lookups", "expectedTime.csv.default")
     if not os.path.isfile(lookupfile):
         if os.path.isfile(defaultfile):
             shutil.copyfile(defaultfile, lookupfile)
