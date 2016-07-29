69c7281736dccc5202ca3bd1c993d7120d051938
tim <tim@hurricanelabs.com>
2016-07-29 12:40:07 -0400

    rename lookup from .sample to .default

--- bin/first_run.py
+++ bin/first_run.py
@@ -3,8 +3,8 @@ import shutil
 splunk_home = os.environ.get("SPLUNK_HOME")
 if splunk_home:
     lookupfile = os.path.join(splunk_home, "etc", "apps", "broken_sources", "lookups", "expectedTime.csv")
-    samplefile = os.path.join(splunk_home, "etc", "apps", "broken_sources", "lookups", "expectedTime.csv.sample")
+    defaultfile = os.path.join(splunk_home, "etc", "apps", "broken_sources", "lookups", "expectedTime.csv.default")
     if not os.path.isfile(lookupfile):
-        if os.path.isfile(samplefile):
-            shutil.copyfile(samplefile, lookupfile)
+        if os.path.isfile(defaultfile):
+            shutil.copyfile(defaultfile, lookupfile)
 
