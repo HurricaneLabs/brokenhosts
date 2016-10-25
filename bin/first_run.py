5c7b8946b30c4686312f7ffba9cf3d16d3ca7091
Cameron Schmidt <cschmidt@hurricanelabs.com>
2016-10-25 13:35:59 -0400

    first_run.py adds sourcetype field if it isn't there

--- bin/first_run.py
+++ bin/first_run.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import csv
 splunk_home = os.environ.get("SPLUNK_HOME")
 if splunk_home:
     lookupfile = os.path.join(splunk_home, "etc", "apps", "broken_hosts", "lookups", "expectedTime.csv")
@@ -7,3 +8,19 @@ if splunk_home:
     if not os.path.isfile(lookupfile):
         if os.path.isfile(defaultfile):
             shutil.copyfile(defaultfile, lookupfile)
+    else:
+        expected_fields = ["index","sourcetype","host","lateSecs","suppressUntil","contact","comments"]
+        current_fields = []
+        tempfile = os.path.join(splunk_home, "etc", "apps", "broken_hosts", "lookups", "expectedTime.csv.tmp")
+        f=open(lookupfile, "r")
+        reader = csv.DictReader(f)
+        current_fields = reader.fieldnames
+        if current_fields != expected_fields:
+            with open(tempfile, "w") as f_temp:
+                writer = csv.DictWriter(f_temp, expected_fields)
+                writer.writeheader()
+                for line in reader:
+                    writer.writerow(line)
+            shutil.copyfile(tempfile, lookupfile)
+        f.close()
+
