b550330fb9c684e1f4749bbbe83ba276071e8fd7
tim <tim@hurricanelabs.com>
2017-08-21 09:39:41 -0400

    update setup to be cleaner

--- appserver/static/setup.js
+++ appserver/static/setup.js
@@ -1,13 +1,5 @@
-(function() {
-  // breakdown the URL into app name and page name
-  var urlAppComponents = (function() {
-    var comps = (location.pathname.split('?')[0]).split('/');
-    var idx   = comps.indexOf('app');
-    return [comps[idx + 1], comps[idx + 2]];
-  })();
+var comps = (location.pathname.split('?')[0]).split('/');
+var idx = comps.indexOf('app');
+var appName = comps[idx + 1];
 
-  // obtain values from previous anonymous function
-  var appName  = urlAppComponents[0];
-  var pageName = urlAppComponents[1];
-
-  window.location = "/manager/" + $appName + "/apps/local/" + $appName + "/setup?action=edit"
+window.location = "/manager/" + appName + "/apps/local/" + appName + "/setup?action=edit";
