8ba12c8be339d8737977a5a9984ba78b24ae0f4b
ian <ian@hurricanelabs.com>
2020-01-03 13:30:37 -0500

    More pylinting

--- bin/setup_endpoint.py
+++ bin/setup_endpoint.py
@@ -1,3 +1,7 @@
+"""
+Populates KV Store with default values
+"""
+
 import json
 import os
 import csv
@@ -5,8 +9,6 @@ import csv
 # pylint: disable=import-error
 import splunk.admin as admin
 import splunk.rest
-from splunk import ResourceNotFound
-
 
 
 class BrokenHostsSetup(admin.MConfigHandler):
@@ -15,19 +17,24 @@ class BrokenHostsSetup(admin.MConfigHandler):
     """
 
     def setup(self):
+        """
+        super() method - not used
+        """
         pass
 
     def handleList(self, _):  # pylint: disable=invalid-name
+        """
+        super() method - not used
+        """
         pass
 
-    '''
-    After user clicks Save on setup page, take updated parameters,
-    normalize them, and save them somewhere
-    '''
     def handleEdit(self, _):  # pylint: disable=invalid-name
+        """
+        super() method - handles populating KVStore with default values
+        """
 
         session_key = self.getSessionKey()
-        
+
         splunk_home = os.environ.get("SPLUNK_HOME")
         default_file = os.path.join(
             splunk_home,
@@ -37,13 +44,16 @@ class BrokenHostsSetup(admin.MConfigHandler):
             "default",
             "data",
             "expectedTime.csv.default")
-        with open(default_file, "r") as f:
-            reader = csv.DictReader(f)
+        with open(default_file, "r") as file:
+            reader = csv.DictReader(file)
             for line in reader:
                 self.write_line(session_key, line)
 
     @staticmethod
     def write_line(session_key, line):
+        """
+        Writes line to KVStore, called in handleEdit()
+        """
         splunk.rest.simpleRequest(
             '/servicesNS/nobody/broken_hosts/storage/collections/data/expectedTime',
             method='POST',
