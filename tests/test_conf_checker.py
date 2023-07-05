7b0424fd4ef29b0f053c708833de0f05ea349dad
Ian Gillespie <iangillespie@Ians-MacBook-Pro.local>
2023-07-05 15:03:18 -0400

    Fix file path

--- tests/test_conf_checker.py
+++ tests/test_conf_checker.py
@@ -8,7 +8,7 @@ import addonfactory_splunk_conf_parser_lib as conf_parser
 class BrokenHostsConfigTest(unittest.TestCase):
 
     def test_confirm_all_saved_searches_exist(self):
-        with open(os.path.join(os.path.abspath('..'), "default/savedsearches.conf"), "r") as f:
+        with open(os.path.join(os.path.abspath('..'), "broken_hosts/default/savedsearches.conf"), "r") as f:
             conf = f.read()
 
         expected_saved_searches = "['Broken Hosts - Auto Sort', 'Broken Hosts Alert Search', 'Broken Hosts Alert - by contact', 'bh_stats_gen', 'Broken Hosts Alert - Volume Based Alerting', 'Broken Hosts Alert Search - Volume Alerting with Seasonality']"
